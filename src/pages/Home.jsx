c7d0a56c9d0eb11535f33b2761f86df4a82aa80d
David Emmanuel <eclefzy@gmail.com>
2025-09-16 17:04:47 +0100

    corrected the navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,40 +7,6 @@ const Home = () => {
         <input type="search" name="search" id="search" placeholder=" Search..." style={{width: "35rem", height: "2.0rem"}}/>
         <button className="btn btn-primary" onClick={() => {}} style={{height: "2.1rem", padding: "0 1rem"}}>Search</button>
       </div>
-      {/* <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 col-sm-6 col-md-4">
-            <button>Button 1</button>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4">
-            <button>Button 2</button>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4">
-            <button>Button 3</button>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4">
-            <button>Button 4</button>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4">
-            <button>Button 5</button>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4">
-            <button>Button 6</button>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4">
-            <button>Button 7</button>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4">
-            <button>Button 8</button>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4">
-            <button>Button 9</button>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4">
-            <button>Button 10</button>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const NotFound = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.error(
+      "404 Error: User attempted to access non-existent route:",
+      location.pathname
+    );
+  }, [location.pathname]);
   return (
-    <div>
-      <h2>404 - Not Found</h2>
-      <p>The page you are looking for does not exist.</p>
+    <div className="container-fluid text-center mt-5 mb-5 py-5">
+      <div className="container p-5">
+        <h1 className="text-center display-1 mb-4">404</h1>
+        <p className="text-center mb-4 text-danger text-xl">
+          Oops! Page Not Found
+        </p>
+        <p className="mb-4">
+          The page you are looking for does not exist or has been moved.
+        </p>
+        <a href="/" className="btn btn-primary mt-3 px-5">
+          Go Home
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default NotFound;
