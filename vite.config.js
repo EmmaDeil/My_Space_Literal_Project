aa3f563707296c3a9a59602b432ffe8a1d21e06d
David Emmanuel <eclefzy@gmail.com>
2025-09-16 17:37:59 +0100

    edited the port description

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  port: 3000,
+  server: {
+    host: "::",
+    port: 3320,
+  },
   plugins: [react()],
   base: '/'
 })
