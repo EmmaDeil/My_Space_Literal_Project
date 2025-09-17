d9c942b70e68c4ee4f96cad03bd2a1ecaac1ee28
David Emmanuel <eclefzy@gmail.com>
2025-09-17 22:17:21 +0100

    added an avatar dropdown to the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,8 +8,15 @@ const Navbar = () => {
             <img src="/logo.ico" alt="Logo" className='navbar-logo' />
             <span className='navbar-title'>SpaceXplorer</span>
             </div>
-            <div className='navbar-links'>
-               <img src="/avatar.png" alt="Avatar" className='navbar-icon' />
+            <div className='navbar-item dropdown' style={{display: "flex", alignItems: "center", gap: "0.5rem"}}>
+               <img src="/avatar.png" alt="Avatar" className='avatar' />
+               <a href="#" className='navbar-link dropdown-toggle' data-bs-toggle="dropdown" aria-expanded="false"></a>
+               <ul className='dropdown-menu dropdown-menu-end'>
+                  <li><a href="#" className='dropdown-item'>Profile</a></li>
+                  <li><a href="#" className='dropdown-item'>Settings</a></li>
+                  <li><hr className='dropdown-divider'/></li>
+                  <li><a href="#" className='dropdown-item'>Logout</a></li>
+               </ul>
                </div>
          </nav>
       </div>
--- src/forms/LoginForm.jsx
+++ src/forms/LoginForm.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const LoginForm = () => {
   return (
     <div>
-      <div className='border p-4 m-4'>
-      <form className='justify-content-center align-items-center d-flex flex-column gap-3' action="">
-        <div className='mb-3'>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" placeholder='Email Address' required />
-        </div>
-        <div className='mb-3'>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" placeholder='*******' required />
-        </div>
-        <button type="submit" className='btn btn-primary' id='loginbtn'>Login</button>
-      </form>
+      <div className="border p-4 m-4">
+        <form
+          className="justify-content-center align-items-center d-flex flex-column gap-3"
+          action=""
+        >
+          <div className="mb-3">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email Address"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="*******"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary" id="loginbtn">
+            Login
+          </button>
+        </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default LoginForm;
